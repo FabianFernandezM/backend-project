beb8837c496517490b1c984bb1f2e3bfecbebb2a
Fabian <xlyncex@gmail.com>
2024-04-15 12:06:21 +0100

    Added /api/topics get endpoint with testing suite, controller and model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,9 +9,30 @@ afterAll(() => db.end());
 
 describe("/api/topics", () => {
     test("GET 200: Should return an array", () => {
+        return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({body}) => {
+            expect(Array.isArray(body)).toBe(true)
+        })
     })
     test("GET 200: Should return an array of topic objects", () => {
+        const topics = require(`${__dirname}/../db/data/test-data/topics.js`)
+        return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({body}) => {
+            expect(body.length).toBe(topics.length)
+            expect(body).toEqual(topics)
+        })
     })
-    test("GET 404: Should return a 'Not found' is the path is incorrect", () => {
+    test("GET 404: Should return a 'Path not found' is the path is incorrect", () => {
+        return request(app)
+        .get("/api/incorrectPath")
+        .expect(404)
+        .then(({body}) => {
+            const {message} = body
+            expect(message).toBe("Path not found")
+        })
     })
 })
--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require("express")
+const { getTopics } = require("./controllers/topics_controllers")
 const app = express()
+
+
+app.get("/api/topics", getTopics)
+
+
+app.all('*', (req, res, next) => {
+    res.status(404).send({message: "Path not found"})
+})
 
 module.exports = app
