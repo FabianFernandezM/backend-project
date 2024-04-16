af44b1f566f1e0bb52cebe07f5c3c8c57982a7db
Fabian <xlyncex@gmail.com>
2024-04-16 13:55:03 +0100

    Refactored models to async await

--- models/articles_models.js
+++ models/articles_models.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection")
 
-function fetchArticles() {
+async function fetchArticles() {
     const sqlStr = `SELECT articles.author, articles.title, articles.article_id,
     articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
     COUNT(comments.comment_id) AS comment_count
@@ -10,37 +10,29 @@ function fetchArticles() {
     GROUP BY articles.article_id
     ORDER BY articles.created_at DESC;`
 
-    return db.query(sqlStr)
-    .then((articles) => {
-        return articles.rows
-    })
+    const articles = await db.query(sqlStr)
+    return articles.rows
 }
 
-function fetchArticleById(article_id) {
-    return db.query(`SELECT author, title, article_id, body, topic, created_at, votes, article_img_url
+async function fetchArticleById(article_id) {
+    const article = await db.query(`SELECT author, title, article_id, body, topic, created_at, votes, article_img_url
     FROM articles
     WHERE article_id = $1;`, [article_id])
-    .then((article) => {
-        if (article.rows.length === 0) return Promise.reject({status: 404, message: "This article does not exist"})
-        return article.rows[0]
-    })
+    if (article.rows.length === 0) return Promise.reject({ status: 404, message: "This article does not exist" })
+    return article.rows[0]
 }
 
-function fetchArticleCommentsById(article_id) {
-    return db.query(`SELECT comment_id, votes, created_at, author, body, article_id
+async function fetchArticleCommentsById(article_id) {
+    const comments = await db.query(`SELECT comment_id, votes, created_at, author, body, article_id
     FROM comments
     WHERE article_id = $1
     ORDER BY created_at DESC;`, [article_id])
-    .then((comments) => {
-        return comments.rows
-    })
+    return comments.rows
 }
 
-function insertArticleCommentById(article_id, newComment) {
-    return db.query(`INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *;`, [newComment.username, newComment.body, article_id])
-    .then((comment) => {
-        return comment.rows[0]
-    })
+async function insertArticleCommentById(article_id, newComment) {
+    const comment = await db.query(`INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *;`, [newComment.username, newComment.body, article_id])
+    return comment.rows[0]
 }
 
 module.exports = {fetchArticleById, fetchArticles, fetchArticleCommentsById, insertArticleCommentById}
