2ba97f2b603932733b7d93ecce71a22042973eab
Fabian <xlyncex@gmail.com>
2024-04-18 14:56:35 +0100

    Refactored app.js to allow routing

---
+++ routes/api-router.js
@@ -0,0 +1,16 @@
+const apiRouter = require('express').Router();
+const usersRouter = require('./users-router')
+const articlesRouter = require('./articles-router')
+const topicsRouter = require('./topics-router')
+const commentsRouter = require('./comments-router')
+const { getEndpoints } = require("../controllers/topics_controllers")
+
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/articles', articlesRouter);
+apiRouter.use('/topics', topicsRouter);
+apiRouter.use('/comments', commentsRouter);
+
+apiRouter.route("/")
+.get(getEndpoints)
+
+module.exports = apiRouter;
---
+++ routes/articles-router.js
@@ -0,0 +1,15 @@
+const articlesRouter = require("express").Router()
+const { getArticleById, getArticles, getArticleCommentsById, postArticleCommentById, patchArticleById } = require("../controllers/articles_controllers")
+
+articlesRouter.route("/")
+.get(getArticles);
+
+articlesRouter.route("/:article_id")
+.get(getArticleById)
+.patch(patchArticleById);
+
+articlesRouter.route("/:article_id/comments")
+.get(getArticleCommentsById)
+.post(postArticleCommentById);
+
+module.exports = articlesRouter;
